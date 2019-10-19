38cdb7b67dc4f96645e7f75a0435fff489bb993e
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-19 18:25:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,4 +14,6 @@ echo 'Channel Creation Taking Place..'
 
 sudo docker exec -it cli peer channel create -o orderer.cool-network.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/cool-network.com/orderers/orderer.cool-network.com/msp/tlscacerts/tlsca.cool-network.com-cert.pem -c mychannel -f ./channel-artifacts/channel.tx
 
+sleep 2
+sudo docker exec -it cli peer channel join -b mychannel.block
 exit 1
