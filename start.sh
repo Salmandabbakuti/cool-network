9f3fba7d81ea16632671baaff97bd366f8a62dda
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-19 18:10:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,3 +9,9 @@ sudo docker network prune
 sleep 3
 
 sudo docker-compose -f docker-compose-cli.yaml up -d
+
+echo 'Channel Creation Taking Place..'
+
+sudo docker exec -it cli peer channel create -o orderer.cool-network.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/cool-network.com/orderers/orderer.cool-network.com/msp/tlscacerts/tlsca.cool-network.com-cert.pem -c publicChannel -f ./channel-artifacts/channel.tx
+
+exit 1
