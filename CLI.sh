65086d434020d5662ac8bf95180e6c839ad931e1
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-19 22:02:05 +0530

    Update CLI.sh

--- CLI.sh
+++ CLI.sh
@@ -1,58 +1,50 @@
-echo "A0=Apple peer0"
-echo " "
-echo "A1=Apple peer1"
-echo " "
-echo "A2=Apple peer2"
-echo " "
-
-echo "M0=Microsoft peer0"
-echo " "
-echo "M1=Microsoft peer1"
-echo " "
-echo "M2=Microsoft peer2"
-echo " "
-echo "Which peer You wanted To Switch CLI Container??"
-read input
-if [ $input == "M1"]; then
-        echo "Setting Up CLI for Microsoft Peer1"
-
-sudo docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/users/Admin@microsoft.cool-network.com/msp" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/peers/peer1.microsoft.cool-network.com/tls/ca.crt" -e "CORE_PEER_ADDRESS=peer1.microsoft.cool-network.com:7051" -e "CORE_PEER_LOCALMSPID=MicrosoftMSP" cli
-fi
-
-if [ $input == "M2"]; then
- 
-       echo "Setting Up CLI for Microsoft Peer2"
-sudo docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/users/Admin@microsoft.cool-network.com/msp" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/peers/peer2.microsoft.cool-network.com/tls/ca.crt" -e "CORE_PEER_ADDRESS=peer2.microsoft.cool-network.com:7051" -e "CORE_PEER_LOCALMSPID=MicrosoftMSP" cli
-
-fi
-
-if [ $input == "A0"]; then
-        echo "Setting Up CLI for Apple Peer0"
-sudo docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/users/Admin@apple.cool-network.com/msp" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/peers/peer0.apple.cool-network.com/tls/ca.crt" -e "CORE_PEER_ADDRESS=peer0.apple.cool-network.com:7051" -e "CORE_PEER_LOCALMSPID=AppleMSP" cli
-fi
-
-if [ $input == "A1"]; then
-        echo "Setting Up CLI for Apple Peer1"
-sudo docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/users/Admin@apple.cool-network.com/msp" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/peers/peer1.apple.cool-network.com/tls/ca.crt" -e "CORE_PEER_ADDRESS=peer1.apple.cool-network.com:7051" -e "CORE_PEER_LOCALMSPID=AppleMSP" cli
-fi
-
-if [ $input == "A2"]; then
-        echo "Setting Up CLI for Apple Peer2"
-
-sudo docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/users/Admin@apple.cool-network.com/msp" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/peers/peer2.apple.cool-network.com/tls/ca.crt" -e "CORE_PEER_ADDRESS=peer2.apple.cool-network.com:7051" -e "CORE_PEER_LOCALMSPID=AppleMSP" cli
-fi
-
-if [ $input == "M0"]; then
-        echo "Setting Up CLI for Microsoft Peer0"
- 
-sudo docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/users/Admin@microsoft.cool-network.com/msp" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/peers/peer0.microsoft.cool-network.com/tls/ca.crt" -e """CORE_PEER_ADDRESS=peer0.microsoft.cool-network.com:7051" -e "CORE_PEER_LOCALMSPID=MicrosoftMSP" cli
+## Environment Variables for Switching CLI Container
 
 
-else
-        echo "Sorry I don't Understand. Choose Right one from Options.."
+>Should be be run from CLI Container ```docker exec -it cli bash```
+
+```
+### Microsoft Peer0
+
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/users/Admin@microsoft.cool-network.com/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/peers/peer0.microsoft.cool-network.com/tls/ca.crt 
+export CORE_PEER_ADDRESS=peer0.microsoft.cool-network.com:7051
+export CORE_PEER_LOCALMSPID=MicrosoftMSP" cli
+
+### Microsoft Peer1
+
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/users/Admin@microsoft.cool-network.com/msp 
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/peers/peer1.microsoft.cool-network.com/tls/ca.crt 
+export CORE_PEER_ADDRESS=peer1.microsoft.cool-network.com:7051
+export CORE_PEER_LOCALMSPID=MicrosoftMSP" cli
+
+### Microsoft Peer2
+
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/users/Admin@microsoft.cool-network.com/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/microsoft.cool-network.com/peers/peer2.microsoft.cool-network.com/tls/ca.crt 
+export CORE_PEER_ADDRESS=peer2.microsoft.cool-network.com:7051
+export CORE_PEER_LOCALMSPID=MicrosoftMSP" cli
+
+### Apple Peer0
+
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/users/Admin@apple.cool-network.com/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/peers/peer0.apple.cool-network.com/tls/ca.crt 
+export CORE_PEER_ADDRESS=peer0.apple.cool-network.com:7051
+export CORE_PEER_LOCALMSPID=AppleMSP" cli
+
+### Apple Peer1
+
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/users/Admin@apple.cool-network.com/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/peers/peer1.apple.cool-network.com/tls/ca.crt 
+export CORE_PEER_ADDRESS=peer1.apple.cool-network.com:7051
+export CORE_PEER_LOCALMSPID=AppleMSP" cli
+
+### Apple Peer2
+
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/users/Admin@apple.cool-network.com/msp
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/apple.cool-network.com/peers/peer2.apple.cool-network.com/tls/ca.crt 
+export CORE_PEER_ADDRESS=peer2.apple.cool-network.com:7051
+export CORE_PEER_LOCALMSPID=AppleMSP" cli
 
 
-
-fi
-
-exit 1
+```
