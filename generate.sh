2921af8c9c0c4518a2b46b371679d23f5e510bf8
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-19 18:15:12 +0530

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -9,13 +9,13 @@ echo 'Generating Certificates and Channel configurations..'
 
 cryptogen generate --config crypto-config.yaml --output=crypto-config
 configtxgen -profile OrdererGenesis -outputBlock ./channel-artifacts/genesis.block
-configtxgen -profile channelAll -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID publicChannel
+configtxgen -profile channelAll -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
 sleep 5
 
 echo 'Updating Anchor Peers for Channel..'
 
-configtxgen -profile channelAll -outputAnchorPeersUpdate ./channel-artifacts/MicrosoftAnchor.tx -channelID publicChannel -asOrg MicrosoftMSP
-configtxgen -profile channelAll -outputAnchorPeersUpdate ./channel-artifacts/AppleAnchor.tx -channelID publicChannel -asOrg AppleMSP
+configtxgen -profile channelAll -outputAnchorPeersUpdate ./channel-artifacts/MicrosoftAnchor.tx -channelID mychannel -asOrg MicrosoftMSP
+configtxgen -profile channelAll -outputAnchorPeersUpdate ./channel-artifacts/AppleAnchor.tx -channelID mychannel -asOrg AppleMSP
 
 echo 'All Done..'
 exit 1
