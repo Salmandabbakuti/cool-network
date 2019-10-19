9327df41281089e7bd480da934598a88ae71f26f
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-19 18:47:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,10 +15,10 @@ sleep 10
 
 echo 'Channel Creation Taking Place..'
 
-sudo docker exec -it cli peer channel create -o orderer.cool-network.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/cool-network.com/orderers/orderer.cool-network.com/msp/tlscacerts/tlsca.cool-network.com-cert.pem -c mychannel -f ./channel-artifacts/channel.tx
+sudo docker exec -it peer0.microsoft.cool-network.com peer channel create -o orderer.cool-network.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/cool-network.com/orderers/orderer.cool-network.com/msp/tlscacerts/tlsca.cool-network.com-cert.pem -c mychannel -f ./channel-artifacts/channel.tx
 
 sleep 2
-sudo docker exec -it cli peer channel join -b mychannel.block
+sudo docker exec -it peer0.microsoft.cool-network.com peer channel join -b mychannel.block
 
 echo 'Exporting channel block to other Peer Containers..'
 
@@ -30,7 +30,7 @@ sudo docker cp mychannel.block peer0.apple.cool-network.com:/opt/gopath/src/gith
 sudo docker cp mychannel.block peer1.apple.cool-network.com:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block
 sudo docker cp mychannel.block peer2.apple.cool-network.com:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block
 sleep 5
-echo 'Joing Peers..'
+echo 'Joining Peers..'
 sudo docker exec -it peer1.microsoft.cool-network.com peer channel join -b mychannel.block
 sudo docker exec -it peer2.microsoft.cool-network.com peer channel join -b mychannel.block
 sudo docker exec -it peer0.apple.cool-network.com peer channel join -b mychannel.block
