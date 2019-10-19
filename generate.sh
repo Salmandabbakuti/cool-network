b2845cff6bce3b98d934d30bb06bd2a06fe4d537
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-10-19 15:49:04 +0530

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 rm -rf channel-artifacts
+rm -rf crypto-config
 mkdir channel-artifacts
 export PATH=${PWD}/bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=$PWD
@@ -8,7 +9,7 @@ echo 'Generating Certificates and Channel configurations..'
 
 cryptogen generate --config crypto-config.yaml --output=crypto-config
 configtxgen -profile OrdererGenesis -outputBlock ./channel-artifacts/genesis.block
-configtxgen -profile channelAll -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID publiChannel
+configtxgen -profile channelAll -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID publicChannel
 sleep 5
 
 echo 'Updating Anchor Peers for Channel..'
